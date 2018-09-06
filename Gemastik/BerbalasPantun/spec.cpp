2523477f58b41ae420d49d3c26f83333a56118f0
Mr Plankton <ajisubarkah99@gmail.com>
2018-09-06 14:21:15 +0700

    Initial commit :tada:

---
+++ Gemastik/BerbalasPantun/spec.cpp
@@ -0,0 +1,43 @@
+#include <tcframe/spec.hpp>
+using namespace tcframe;
+
+class ProblemSpec : public BaseProblemSpec {
+protected:
+    int lengthArray;
+    vector<int> array;
+    int adjacent;
+
+    void InputFormat() {
+        LINE(lengthArray, array % SIZE(lengthArray));
+    }
+
+    void OutputFormat() {
+        LINE(adjacent);
+    }
+
+    void GradingConfig() {
+        TimeLimit(0.5);
+        MemoryLimit(64);
+    }
+
+    void Constraints() {
+        CONS(2 <= lengthArray && lengthArray <= 10);        
+    }
+};
+
+class TestSpec : public BaseTestSpec<ProblemSpec> {
+protected:
+    void SampleTestCase1() {
+        Input({
+            "6 3 6 -2 -5 7 3"
+        });
+        Output({
+            "21"
+        });
+    }
+
+    void TestCases() {
+        CASE(lengthArray = 2, array = {-1,-2});
+        CASE(lengthArray = 6, array = {5, 1, 2, 3, 1, 4});
+    }
+};
