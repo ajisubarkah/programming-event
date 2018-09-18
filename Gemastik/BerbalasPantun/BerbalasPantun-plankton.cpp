03a199f123ef785c5d398696120dfb7b396bacf3
Mr Plankton <ajisubarkah99@gmail.com>
2018-09-18 13:12:53 +0700

    Fix time limit

--- Gemastik/BerbalasPantun/BerbalasPantun-plankton.cpp
+++ Gemastik/BerbalasPantun/BerbalasPantun-plankton.cpp
@@ -4,27 +4,21 @@
 
 using namespace std;
 
-int berbalasPantun(int N, int A[], int B[])
-{
-    int total = 0;
-    for (int i = 0; i < N; i++)
-    {
-        for (int j = 0; j < N; j++)
-        {
-            total = total + (A[i] + B[i]);
-        }
-    }
-    return total;
-}
-
 main()  
 {
     int N;
     cin >> N;
     int A[N], B[N];
-    
-    for(int i = 0; i < N; i++) cin >> A[i];
-    for(int i = 0; i < N; i++) cin >> B[i];
 
-    cout << berbalasPantun(N, A, B) << endl;
+    long total = 0;
+
+    for(int i = 0; i < N; i++) cin >> A[i];
+    
+    for(int i = 0; i < N; i++){ 
+        cin >> B[i];
+        for(int j = 0; j < N; j++)
+            total += B[i] + A[j];
+    }
+
+    cout << total << endl;
 }
--- Gemastik/MembangunMenara/MembangunMenara-plankton.cpp
+++ Gemastik/MembangunMenara/MembangunMenara-plankton.cpp
@@ -5,31 +5,29 @@
 
 using namespace std;
 
-vector<int> membangunMenara(int n, int a[], int b[], int c[]){
+vector<int> membangunMenara(int n, int a[], int b[], int c[]) {
     vector<int> result;
     int hasil[2] = {0,0};
     for(int i = 0; i < n; i++){
-        hasil[0] += minmax({a[i],b[i],c[i]}).first;
-        hasil[1] += minmax({a[i],b[i],c[i]}).second;
+        hasil[0] += min({a[i],b[i],c[i]});
+        hasil[1] += max({a[i],b[i],c[i]});
     }
     result.push_back(hasil[0]);
     result.push_back(hasil[1]);
     return result;
 }
 
-main()
-{
+int main(){
     int t;
     cin >> t;
     int n, a[t], b[t], c[t];
-    while (t--)
-    {
+    for(int i = 0; i < t; i++) {
         cin >> n;
         for (int i = 0; i < n; i++)
         {
             cin >> a[i] >> b[i] >> c[i];
         }
         vector<int> hasil = membangunMenara(n,a,b,c);
-        printf("%d %d\n", hasil[0], hasil[1]);
+        cout << hasil[0] << " " << hasil[1] << endl;
     }
 }
