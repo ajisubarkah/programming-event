e4a818347d535945e163f44c4f8891b0155ced41
Mr Plankton <ajisubarkah99@gmail.com>
2018-09-23 19:22:21 +0700

    Initial commit :tada:

--- Gemastik/FotograferWisuda/FotograferWisuda-dewnan98.cpp
+++ Gemastik/FotograferWisuda/FotograferWisuda-dewnan98.cpp
@@ -2,59 +2,51 @@
 #include <math.h>
 using namespace std;
 
-
-string fotograferWisuda(float a , float b , float k)
+string fotograferWisuda(float a, float b, float k)
 {
-    string hasil="";
+    string hasil = "";
 
+    float lakiMaksimalDempet = ceil(a / (b + 1));
+    float perempuanMaksimalDempet = ceil(b / (a + 1));
 
-    float lakiMaksimalDempet = ceil( a/(b+1));
-    float perempuanMaksimalDempet = ceil(b/(a+1));
-
-    if(lakiMaksimalDempet>2 ||perempuanMaksimalDempet>k )
+    if (lakiMaksimalDempet > 2 || perempuanMaksimalDempet > k)
         return "mustahil";
-
 
     int laki = a;
     int pere = b;
 
-    int counterLaki=0;
-    int counterPerempuan =0;
+    int counterLaki = 0;
+    int counterPerempuan = 0;
 
-    for(int i = 0 ;i<a+b;i++)
+    for (int i = 0; i < a + b; i++)
     {
-       if(laki>0 && counterLaki<=2 &&  laki>pere ||counterPerempuan==k)
-       {
-            hasil+="L";
+        if (laki > 0 && counterLaki < 2 && laki > pere || counterPerempuan == k)
+        {
+            hasil += "L";
             laki--;
             counterLaki++;
-            counterPerempuan=0;
-       }
+            counterPerempuan = 0;
+        }
 
-
-       else {
-            hasil+="P";
+        else
+        {
+            hasil += "P";
             pere--;
             counterPerempuan++;
             counterLaki = 0;
-       }
-
-
-
+        }
     }
-
     return hasil;
-
 }
 
 main()
 {
-
-    cout<<fotograferWisuda(0,4,4)<<endl;
-    cout<<fotograferWisuda(0,4,3)<<endl;
-    cout<<fotograferWisuda(1,3,3)<<endl;
-    cout<<fotograferWisuda(3,3,3)<<endl;
-    cout<<fotograferWisuda(5,8,2)<<endl;
-    cout<<fotograferWisuda(5,6,1)<<endl;
-    cout<<fotograferWisuda(12,12,1)<<endl;
+    int T;
+    float A, B, K;
+    cin >> T;
+    for (int i = 0; i < T; i++)
+    {
+        cin >> A >> B >> K;
+        cout << fotogragerWisuda(A, B, K) << endl;
+    }
 }
